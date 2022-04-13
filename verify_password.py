2a9cf9ea03ad36261a4859ee6d1b84ba64615d03
Durigy <56241583+Durigy@users.noreply.github.com>
2022-04-13 17:52:18 +0100

    get password

--- verify_password.py
+++ verify_password.py
@@ -1,45 +1,109 @@
-def get_password(pass_count=4):
+import time
+import cv2 as cv
+import mediapipe as mp
+from collections import Counter
+import vision
+
+
+def get_password(mp_hands, mp_draw, hands, pass_count=4):
     pwd_list = []
+    finger_count = 0
     while not len(pwd_list) == pass_count:
-        pwd_list.append()
+        finger_count = vision.get_finger_count()
+        if finger_count != pwd_list[-1]:
+            pwd_list.append(finger_count)
+
     return pwd_list
 
 
-def setUpPassword():
-    pass
 
 
+### old stuff ###
 
-def identifyPassword(fingercount):
-    global password_stack, input_samples
 
-    # collect input samples (for error correction)
-    if len(input_samples) < 4:
-        input_samples.append(fingercount)
-        return
-        
-    # next_digit will be determined based on input_samples
-    next_digit = 0
+# mp_hands = mp.solutions.hands
+# mp_draw = mp.solutions.drawing_utils
+# hands = mp_hands.Hands(static_image_mode=False, max_num_hands = 1, min_detection_confidence=0.70)
 
-    next_digit = Counter(input_samples).most_common(1)[0][0]
-    print(next_digit)
+# cap = cv.VideoCapture(0)
 
-    input_samples = []
+# password = [2, 5, 4]
+# password_stack = []
+# input_samples = []
 
-    if next_digit == 0:
-        password_stack = []
-    else:
-        if len(password_stack) != 0:
-            if next_digit != password_stack[-1]:
-                password_stack.append(next_digit)
-        else:
-            password_stack.append(next_digit)
+# def get_password(pass_count=4):
+#     pwd_list = []
+#     while not len(pwd_list) == pass_count:
+#         pwd_list.append()
+#     return pwd_list
+
+# def setUpPassword():
+#     pass
+
+# def get_password(pass_count=4):
+#     pwd_list = []
+#     while not len(pwd_list) == pass_count:
+#         pwd_list.append()
+
+#         # collect input samples (for error correction)
+#         if len(input_samples) < 4:
+#             input_samples.append(fingercount)
+#             return
             
-    print(password_stack)
+#         # next_digit will be determined based on input_samples
+#         next_digit = 0
 
-    if len(password_stack) == len(password):
-        if password == password_stack:
-            # print('unlocked')
-            # password_stack = []
-            # time.sleep(3)
-            return password_stack
+#         next_digit = Counter(input_samples).most_common(1)[0][0]
+#         print(next_digit)
+
+#         input_samples = []
+
+#         if next_digit == 0:
+#             password_stack = []
+#         else:
+#             if len(password_stack) != 0:
+#                 if next_digit != password_stack[-1]:
+#                     password_stack.append(next_digit)
+#             else:
+#                 password_stack.append(next_digit)
+                
+#         print(password_stack)
+#         return password_stack
+#     return pwd_list
+
+
+
+# def identifyPassword(fingercount):
+#     global password_stack, input_samples
+
+#     # collect input samples (for error correction)
+#     if len(input_samples) < 4:
+#         input_samples.append(fingercount)
+#         return
+        
+#     # next_digit will be determined based on input_samples
+#     next_digit = 0
+
+#     next_digit = Counter(input_samples).most_common(1)[0][0]
+#     print(next_digit)
+
+#     input_samples = []
+
+#     if next_digit == 0:
+#         password_stack = []
+#     else:
+#         if len(password_stack) != 0:
+#             if next_digit != password_stack[-1]:
+#                 password_stack.append(next_digit)
+#         else:
+#             password_stack.append(next_digit)
+            
+#     print(password_stack)
+#     return password_stack
+
+    # if len(password_stack) == len(password):
+    #     if password == password_stack:
+    #         # print('unlocked')
+    #         # password_stack = []
+    #         # time.sleep(3)
+    #         return password_stack
