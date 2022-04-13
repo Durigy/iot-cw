b24726ad2ac6f743e7161a57665116b01bf6dcb9
Durigy <56241583+Durigy@users.noreply.github.com>
2022-04-13 19:59:26 +0100

    ultra sonci sensor func

---
+++ ultra_sonic.py
@@ -0,0 +1,24 @@
+import grovepi
+import time
+
+# set I2C to use the hardware bus
+grovepi.set_bus("RPI_1")
+
+# Connect the Grove Ultrasonic Ranger to digital port D4
+# SIG,NC,VCC,GND
+ultrasonic_ranger = 3
+
+repeat_times = 5
+
+def person_detected():
+    distance = 0
+    for _ in range(repeat_times):
+        try:
+            # Read distance value from Ultrasonic
+            distance += grovepi.ultrasonicRead(ultrasonic_ranger)
+
+        except Exception as e:
+            print ("Error:{}".format(e))
+
+        time.sleep(0.1) # don't overload the i2c bus
+    return abs(distance / repeat_times)
