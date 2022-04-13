c7d6e9cb512a36ee29d66ac69bdb3f51d6296b49
Durigy <56241583+Durigy@users.noreply.github.com>
2022-04-13 16:19:39 +0100

    start of package setup

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    
+import vision
 
+def main():
+    pass
+    
+def setUpPassword():
+    print('Start by making a fist.')
+    while True:
+
+        ret, frame = cap.read()
+        if not ret:
+            continue
+
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        res = hands.process(frame)
+
+        if res.multi_hand_landmarks:
+            fingercount = detectFingersUp(res, frame)
+            if fingercount == 0:
+                pass
+            
+
+        cv.imshow('output', cv.cvtColor(frame, cv.COLOR_RGB2BGR))
+
+        if cv.waitKey(1) == 'q':
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
