1a9e6bcaf4c4cd70d5908d3a86f7a0fca985b719
Durigy <56241583+Durigy@users.noreply.github.com>
2022-04-15 15:16:47 +0100

    button stuff

---
+++ button.py
@@ -0,0 +1,23 @@
+import re
+import time
+import grovepi
+from collections import Counter
+
+# Connect the Grove Button to digital port D3
+# SIG,NC,VCC,GND
+button = 3
+grovepi.pinMode(button,"INPUT")
+
+def read_button():
+    avg_press = []
+    for _ in range(5):
+        try:
+            press = grovepi.digitalRead(button) 
+            print(press)
+            avg_press.append(press)
+            time.sleep(0.05)
+
+        except IOError:
+            print ("Error")
+    return Counter(avg_press).most_common(1)[0][0]
+    
