7f3e84773d2ac2774ac851839bc2de9e8bd757de
Ioannis-Marios Stavropoulos <jstav02@gmail.com>
2022-04-19 20:03:51 +0100

    more base flask

---
+++ website/main/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from main import routes
