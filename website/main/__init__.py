c691a6b65a3e89c5abd00c7a8c9eef5f274429ac
Durigy <56241583+Durigy@users.noreply.github.com>
2022-04-26 00:05:46 +0100

    db setup stuff

--- website/main/__init__.py
+++ website/main/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'passwrd'
-# app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SECRET_KEY'] = '35ae3b45dacdca9eefdc2f657004b9512bfbab4416d9ab44'
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
-# db = SQLAlchemy(app)
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-from main import routes
+from . import routes
