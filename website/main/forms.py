a3798a51edcbed4e981e7b329ec4cc943dfa6223
Durigy <56241583+Durigy@users.noreply.github.com>
2022-04-30 07:03:52 +0100

    that is my web stuff finished

--- website/main/forms.py
+++ website/main/forms.py
@@ -6,7 +6,7 @@ from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email *', validators=[DataRequired(), Email()])
-    password = PasswordField('password *', validators=[DataRequired()]) #, Regexp('^(?=.*\d).{6,8}$', message='Your password should be between 6 and 8 Charaters long and contain at least 1 number')])
+    password = PasswordField('Password *', validators=[DataRequired()]) #, Regexp('^(?=.*\d).{6,8}$', message='Your password should be between 6 and 8 Charaters long and contain at least 1 number')])
     confirm_password = PasswordField('Confirm password *', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
@@ -21,3 +21,7 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password *', validators=[DataRequired(), ]) # Regexp('^(?=.*\d).{6,8}$', message='Your password should be between 6 and 8 Charaters long and contain at least 1 number')
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class ChangeDevicePasswordForm(FlaskForm):
+    password = PasswordField('Password *', validators=[DataRequired(), Length(min=1, max=8)])
+    submit = SubmitField('Update Password')
